f62d516c981ef22fb9e0b2d526b1becfdcaec127
Victor <victormwendwa804@gmail.com>
2024-07-29 09:37:55 +0300

    Print

---
+++ flushing.py
@@ -0,0 +1,6 @@
+import time
+
+# Start a loop
+for i in range(5):
+    print(i, end=' ', flush=True)
+    time.sleep(1)
---
+++ print.py
@@ -0,0 +1,2 @@
+with open('output.txt', 'w') as f:
+    print("Hello, file!", file=f)
