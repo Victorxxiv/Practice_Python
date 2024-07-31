a24a4dec92109c5b9bf7095f6a424a0d2c547ea3
Victor <victormwendwa804@gmail.com>
2024-07-31 10:48:12 +0300

    Conditionals

---
+++ conditionals.py
@@ -0,0 +1,9 @@
+x = int(input("What is x? "))
+y = int(input("What is y? "))
+
+if x < y:
+    print("x is less than y")
+if x > y:
+    print("x is greater than y")
+if x == y:
+    print("x is equal to y")
