a147ae10d00b692565f4d37ae717e3ae1f72d319
Victor <victormwendwa804@gmail.com>
2024-08-06 11:21:05 +0300

    Match

---
+++ house.py
@@ -0,0 +1,14 @@
+name = input("What is your name? ")
+
+if name == "Ragnar":
+    print("Norway")
+elif name == "Floki":
+    print("Norway")
+elif name == "Rollo":
+    print("Norway")
+elif name == "Lagertha":
+    print("Norway")
+elif name == "Jon Snow":
+    print("Winterfell")
+else:
+    print("Who?")
