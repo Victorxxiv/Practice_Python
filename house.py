4134e7ca8b705a270be0614c7f0fd73b9a3e2add
Victor <victormwendwa804@gmail.com>
2024-08-06 11:30:14 +0300

    Match Case|

--- house.py
+++ house.py
@@ -1,13 +1,7 @@
 name = input("What is your name? ")
 
 match name:
-    case "Ragnar":
-        print("Norway")
-    case "Floki":
-        print("Norway")
-    case "Rollo":
-        print("Norway")
-    case "Lagertha":
+    case "Ragnar" | "Floki" | "Rollo" | "Lagertha":
         print("Norway")
     case "Jon Snow":
         print("Winterfell")
