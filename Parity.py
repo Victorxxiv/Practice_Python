39013c2b8740a2ba2d54a9fcfa0c3469416019df
Victor <victormwendwa804@gmail.com>
2024-08-06 09:53:33 +0300

    Bool

--- Parity.py
+++ Parity.py
@@ -1,6 +1,15 @@
-x = int(input("What is x? "))
+def main():
+    x = int(input("What is x? "))
+    if is_even(x):
+        print("Even")
+    else:
+        print("Odd")
 
-if x % 2 == 0:
-    print("Even")
-else:
-    print("Odd")
+def is_even(n):
+    if n % 2 == 0:
+        return True
+    else:
+        return False
+
+
+main()
