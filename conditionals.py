afa8ec78a8b0d3dc136ef03150959b9a57f18798
Victor <victormwendwa804@gmail.com>
2024-08-06 09:09:00 +0300

    elif

--- conditionals.py
+++ conditionals.py
@@ -3,7 +3,7 @@ y = int(input("What is y? "))
 
 if x < y:
     print("x is less than y")
-if x > y:
+elif x > y:
     print("x is greater than y")
-if x == y:
+elif x == y:
     print("x is equal to y")
