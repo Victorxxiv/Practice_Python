03d28a3c6e9822884209e6bd860b5a8fe6059353
Victor <victormwendwa804@gmail.com>
2024-08-09 09:55:55 +0300

    Dict/List

--- List/List.py
+++ List/List.py
@@ -1,8 +1,8 @@
-students = {"Victor": "Nairobi",
-            "Ragnar": "Norway",
-            "Rollo": "Norway",
-            "Jon": "Winterfell"
-}
-
-
-print( students["Victor"])
+students = [
+    {"name": "Victor", "house": "Nairobi", "patronous": "Otter"},
+    {"name": "Ragnar", "house": "Norway", "patronous": "Stag"},
+    {"name": "Rollo", "house": "Norway", "patronous": "Jack Russell"},
+    {"name": "Jon", "house": "Winterfell", "patronous": None}
+]
+for student in students:
+    print(student["name"], student["house"], student["patronous"], sep=", ")
